e6fafe9fbdebc2d89cabde4f3677aaaf31e39515
Griffen Fargo <ghfargo@gmail.com>
2024-06-18 02:00:19 -0400

    Update `layout.tsx` and `package.json` for speed insight integration
    Implemented `SpeedInsights` from `@vercel/speed-insights/next` in `layout.tsx`. Added `@vercel/speed-insights` to dependencies in `package.json`. This integration gives us access to performance insights to aid in application optimization.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { Metadata } from "next"
 import { Inter } from "next/font/google"
@@ -43,6 +44,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font-sans ${inter.variable}`}>{children}</body>
       <Analytics />
+      <SpeedInsights />
     </html>
   )
 }
