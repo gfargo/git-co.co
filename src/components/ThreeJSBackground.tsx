4d21d504e1f29611806ceb5fb77771918f880981
Griffen Fargo <ghfargo@gmail.com>
2024-06-18 20:10:59 -0400

    Update ThreeJSBackground with effects and scrolling camera
    Enhanced the `ThreeJSBackground` component with a floating effect, rotation, and camera alignment for the `MascottPlane`. Also added a `ScrolledCammera` component to update the camera's position based on the window scroll. Modified the `FaqSection` to include a gradient background, adjusted font styles, and introduced enhancements to the accordion. Added hidden image source for future use.

--- src/components/ThreeJSBackground.tsx
+++ src/components/ThreeJSBackground.tsx
@@ -1,37 +1,122 @@
-"use client";
+"use client"
 
-import React, { useRef } from "react"
-import { Canvas, useFrame } from "@react-three/fiber"
-import { OrbitControls, Stars } from "@react-three/drei"
-import { Mesh } from "three"
+import React, { useRef, useState, useEffect } from "react"
+import { Canvas, useFrame, useThree } from "@react-three/fiber"
+import {
+  MeshDistortMaterial,
+  Sparkles,
+  SpotLight,
+  Stars
+} from "@react-three/drei"
+import { Mesh, TextureLoader, Vector3 } from "three"
 
-const Box = () => {
+const MascottPlane = () => {
   const mesh = useRef<Mesh>(null!)
+  const { camera } = useThree()
+  const [texture, setTexture] = useState(null)
+  const initialPosition = useRef(
+    new Vector3(
+      Math.random() * -1 - 1,
+      Math.random() * 1 - 1,
+      Math.random() * 1 - 1
+    )
+  )
+  const time = useRef(0)
 
-  useFrame(() => {
+  useEffect(() => {
+    const randomLetter = ["a", "b", "c", "d"].sort(() => Math.random() - 0.5)[0]
+    new TextureLoader().load(
+      `/mascott/mascott_${randomLetter}.png`,
+      (loadedTexture) => {
+        setTexture(loadedTexture)
+      }
+    )
+  }, [])
+
+  useFrame((state, delta) => {
     if (mesh.current) {
+      time.current += delta
+
+      // Apply floating effect
+      mesh.current.position.x =
+        initialPosition.current.x + Math.sin(time.current) * 0.1
+      mesh.current.position.y =
+        initialPosition.current.y + Math.sin(time.current * 0.1) * 0.1
+      mesh.current.position.z =
+        initialPosition.current.z + Math.sin(time.current * 0.2) * 0.05
+
+      // Rotate
       mesh.current.rotation.x += 0.01
       mesh.current.rotation.y += 0.01
+
+      // Ensure the plane is always facing the camera
+      mesh.current.lookAt(camera.position)
     }
   })
 
   return (
-    <mesh ref={mesh}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={"orange"} />
+    <mesh ref={mesh} position={initialPosition.current}>
+      <planeGeometry args={[1, 1]} />
+      {texture && <meshStandardMaterial map={texture} transparent />}
+      <Sparkles
+        speed={0.2}
+        castShadow
+        noise={Math.cos(-time.current) * 0.1}
+        scale={1.25}
+        count={50}
+        color={"#709e8a"}
+      />
+      <Sparkles
+        speed={0.2}
+        castShadow
+        noise={0.2}
+        scale={1.5}
+        count={50}
+        color={"#709e8a"}
+      />
     </mesh>
   )
 }
 
+const ScrolledCammera = () => {
+  const { camera } = useThree()
+  const [scrollY, setScrollY] = useState(window.scrollY)
+
+  useEffect(() => {
+    camera.position.z = -scrollY * 0.0005
+    camera.lookAt(0, 0, 0)
+  }, [])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY
+      setScrollY(scrollY)
+      camera.position.z = -scrollY * 0.0005
+      camera.lookAt(0, 0, 0)
+    }
+
+    window.addEventListener("scroll", handleScroll)
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [camera])
+
+  return null
+}
+
 const ThreeJSBackground: React.FC = () => {
   return (
-    <Canvas>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Box />
-      <OrbitControls />
-      <Stars />
-    </Canvas>
+    <div className="absolute inset-0 z-0 h-[960px] t-0">
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+
+        <MascottPlane />
+
+        <ScrolledCammera />
+        <Stars speed={0.5} depth={500} fade />
+      </Canvas>
+    </div>
   )
 }
 
